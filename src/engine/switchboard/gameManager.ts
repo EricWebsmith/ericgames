5ecb837e1761138c9c426d4b89af6d449761c041
eric <eric>
2026-04-15 14:40:35 +0800

    remove consoles

--- src/engine/switchboard/gameManager.ts
+++ src/engine/switchboard/gameManager.ts
@@ -4,17 +4,10 @@ export function tranverse(board: Board, startTileIndex: number, startDirection: 
     let currentTileIndex = startTileIndex;
     let currentDirection = startDirection;
     let currentTile = board.tiles[currentTileIndex];
-    console.log("Start Tile", currentTile);
 
     while (true) {
-        console.log("===========================");
-        console.log("Current Tile Index and Direction", currentTileIndex, currentDirection);
-        console.log("currentTile", currentTile);
-
         const outDir = currentTile.arcDict[currentDirection];
-        console.log("outDir", outDir);
         const nextTileIndex = currentTile.edges[outDir];
-        console.log("nextTileIndex", nextTileIndex);
         if (nextTileIndex === undefined) {
             return [currentTileIndex, outDir];
         }
